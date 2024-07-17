ed5da1c00e65b89de317d9b71cd7aac6a999c1ee
Om Rawat <omrawat1805@gmail.com>
2024-07-17 15:03:34 +0530

    1

--- app/src/main/java/com/example/rhythm/Splash.java
+++ app/src/main/java/com/example/rhythm/Splash.java
@@ -1,6 +1,8 @@
 package com.example.rhythm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,17 @@ public class Splash extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.splash);
 
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, loginpage.class));
+            }
+        }, 3000);
+
+
         }
     }
+
+
