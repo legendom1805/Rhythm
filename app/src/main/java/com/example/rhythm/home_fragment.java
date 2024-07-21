1bffec537dd0ed9a00c3437b1437baab08a9ab8b
Om Rawat <omrawat1805@gmail.com>
2024-07-21 23:58:41 +0530

    1

--- app/src/main/java/com/example/rhythm/home_fragment.java
+++ app/src/main/java/com/example/rhythm/home_fragment.java
@@ -1,20 +1,74 @@
 package com.example.rhythm;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.ResultReceiver;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 
-public class home_fragment extends Fragment {
+import com.example.rhythm.recycler_view_categories.categories_adaptor;
+import com.example.rhythm.recycler_view_categories.model;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class   home_fragment extends Fragment {
+
+    ArrayList<model> categoriesarr;
+    categories_adaptor categoriesAdaptor;
+    FirebaseFirestore db;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerviewcontact);
+        recyclerView.setLayoutManager(new GridLayoutManager(requireContext(),2));
+        db = FirebaseFirestore.getInstance();
+        categoriesarr = new ArrayList<model>();
+        categoriesAdaptor = new categories_adaptor(getContext(),categoriesarr );
+        recyclerView.setAdapter(categoriesAdaptor);
+        EventChangeListner();
+        return view;
+    }
+
+    private void EventChangeListner() {
+        db.collection("category")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+
+                        if(error !=null){
+
+                            Log.e("Firestore error",error.getMessage());
+                            return;
+
+                        }
+
+                        for(DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()){
+                            if(dc.getType() == DocumentChange.Type.ADDED){
+                                categoriesarr.add(dc.getDocument().toObject(model.class));
+                            }
+
+                            categoriesAdaptor.notifyDataSetChanged();
+                        }
+
+                    }
+                });
     }
 }
